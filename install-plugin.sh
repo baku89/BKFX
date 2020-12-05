9fe3a09d6e8da6cb9f7822ae4294733660d038fc
Baku Hashimoto <mail@baku89.com>
2020-12-05 15:39:28 +0900

    Fix install-plugin.sh

--- install-plugin.sh
+++ install-plugin.sh
@@ -1,5 +1,4 @@
 # Type a script or drag a script file from your workspace to insert its path.
 
 rm -rf "${PLUGIN_DIR}/${FULL_PRODUCT_NAME}"
-mkdir -p "${PLUGIN_DIR}/${FULL_PRODUCT_NAME}"
-cp -a "${BUILT_PRODUCTS_DIR}/${APPLICATION_NAME}/${FULL_PRODUCT_NAME}" "${PLUGIN_DIR}/${FULL_PRODUCT_NAME}"
+cp -a -f "${BUILT_PRODUCTS_DIR}/${APPLICATION_NAME}/${FULL_PRODUCT_NAME}" "${PLUGIN_DIR}/${FULL_PRODUCT_NAME}"
