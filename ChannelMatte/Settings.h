4ca850ef8cc2b29708707fe4d9a9ea549d2b51f0
Baku Hashimoto <mail@baku89.com>
2020-12-06 20:24:14 +0900

    Change effects' name

--- ChannelMatte/Settings.h
+++ ChannelMatte/Settings.h
@@ -1,4 +1,4 @@
-#define FX_SETTINGS_NAME "ChannelMatte"
-#define FX_SETTINGS_MATCH_NAME "BAKU89 ChannelMatte"
+#define FX_SETTINGS_NAME "BK Channel Matte"
+#define FX_SETTINGS_MATCH_NAME "BAKU89 Channel Matte"
 #define FX_SETTINGS_CATEGORY "Channel"
 #define FX_SETTINGS_DESCRIPTION "(c) 2020 Baku Hashimoto"
--- RichterStrip/Settings.h
+++ RichterStrip/Settings.h
@@ -1,4 +1,4 @@
-#define FX_SETTINGS_NAME "RichterStrip"
-#define FX_SETTINGS_MATCH_NAME "BAKU89 RichterStrip"
+#define FX_SETTINGS_NAME "BK Richter Strip"
+#define FX_SETTINGS_MATCH_NAME "BAKU89 Richter Strip"
 #define FX_SETTINGS_CATEGORY "Distort"
 #define FX_SETTINGS_DESCRIPTION "(c) 2020 Baku Hashimoto"
