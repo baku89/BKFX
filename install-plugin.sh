7934a42f318a9f6d8cc7500b729ec2a51efe6c0c
Baku Hashimoto <mail@baku89.com>
2020-12-06 20:33:40 +0900

    Add Build All target

--- install-plugin.sh
+++ install-plugin.sh
@@ -1,4 +1,3 @@
-# Type a script or drag a script file from your workspace to insert its path.
-
+mkdir -p "${PLUGIN_DIR}"
 rm -rf "${PLUGIN_DIR}/${FULL_PRODUCT_NAME}"
 cp -a -f "${BUILT_PRODUCTS_DIR}/${APPLICATION_NAME}/${FULL_PRODUCT_NAME}" "${PLUGIN_DIR}/${FULL_PRODUCT_NAME}"
